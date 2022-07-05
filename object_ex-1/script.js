13a5b795327940b2b824be303ec7bffb0f004c12
David Zuk <davidzuk94@gmail.com>
2022-07-05 16:45:53 +0300

    homework

---
+++ object_ex-1/script.js
@@ -0,0 +1,100 @@
+console.log({
+  securedBuilding: {
+    pass: {
+      passKey0: {
+        employee: "Alina",
+        sex: "female",
+        startDate: "2013-07-15",
+        status: "active",
+        room: [1, 2, 6, 8],
+      },
+      passKey1: {
+        employee: "Natan",
+        sex: "male",
+        startDate: "2017-04-05",
+        status: "active",
+        room: [0, 1, 2, 3, 4, 6, 7],
+      },
+      passKey2: {
+        employee: "Yony",
+        sex: "male",
+        startDate: "2015-11-21",
+        status: "active",
+        room: [1, 2, 4, 5],
+      },
+      passKey3: {
+        employee: "Elad",
+        sex: "male",
+        startDate: "2018-06-08",
+        status: "active",
+        room: [1, 2, 6, 7, 9],
+      },
+      passKey4: {
+        employee: "Alex",
+        sex: "male",
+        startDate: "2020-02-14",
+        status: "active",
+        room: [1, 2, 4, 5],
+      },
+      passKey5: {
+        employee: "Sergey",
+        sex: "male",
+        startDate: "2019-08-27",
+        status: "active",
+        room: [0, 1, 2, 3, 4, 6, 7],
+      },
+      passKey6: {
+        employee: "victoria",
+        sex: "female",
+        startDate: "2021-02-11",
+        status: "active",
+        room: [1, 2, 6, 10],
+      },
+      passKey7: {
+        employee: "That guy",
+        sex: "male",
+        startDate: null,
+        status: undefined,
+        room: [11],
+      },
+    },
+    room: {
+      door0: {
+        goesTo: "main entrance",
+      },
+      door1: {
+        goesTo: "employee lounge",
+      },
+      door2: {
+        goesTo: "kitchen",
+      },
+      door3: {
+        goesTo: "security room",
+      },
+      door4: {
+        goesTo: "server room",
+      },
+      door5: {
+        goesTo: "laboratory",
+      },
+      door6: {
+        goesTo: "conference room",
+      },
+      door7: {
+        goesTo: "2nd floor",
+      },
+      door8: {
+        goesTo: "office 1",
+      },
+      foor9: {
+        goesTo: "office 2",
+      },
+      door10: {
+        goesTo: " office3",
+      },
+      door11: {
+        goesTo: "D-day button",
+      },
+    },
+  },
+});
